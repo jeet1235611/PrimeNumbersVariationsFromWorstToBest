de08053234e397183524fa9f2a4a7b3808f183af
acoustic_jeet <js.jitendra11@gmail.com>
2018-04-24 10:26:17 +0530

    Create prime5.cpp

---
+++ prime5.cpp
@@ -0,0 +1,42 @@
+//Prime number between 1 and 100
+
+#include <iostream>
+using namespace std;
+bool isPrime(int n){
+        int i;
+        if(n == 1)
+                return false;
+        if(n == 2 || n == 3){
+                cout << n << endl;
+                return true;
+        }
+        for(i = 2; i <= n/2; i++){
+                if(n % i == 0){
+                        //break;
+                        return false;
+                }
+        }
+        cout << n << endl;
+        return true;
+}
+int main(){
+        int low, high;
+        //Prime number between 60 to 100
+        cout << "Enter the two values in between which the prime number has to be calculated\n";
+        cin >> low >> high;
+
+
+        int count = 0;
+        bool n = false;
+
+        for(int i = low; i >= 0; i++){
+                n = isPrime(i);
+                low++;
+                //if(n)
+                //count++;
+
+
+                if(low == high)
+                        return 0;
+        }
+}
