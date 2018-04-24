f6b30c6f7b4bb9b0e48e6dad3b8e738107775825
acoustic_jeet <js.jitendra11@gmail.com>
2018-04-24 06:30:26 +0530

    Create prime2.cpp

---
+++ prime2.cpp
@@ -0,0 +1,31 @@
+//Isme loop ko n/2 tak run karwaya h mene instead of n
+
+#include <iostream>
+using namespace std;
+int isPrime(int n){
+        int i;
+        if(n == 0 || n == 1){
+                cout << "Not Prime" << endl << n << endl;
+                return 0;
+        }
+        if(n == 2){
+                cout << "Prime = Yes "<< n << endl;
+                return 0;
+        }
+        for(i = 2; i < n/2; i++){
+                if(n % i == 0){
+                        cout << "NOT prime" << endl;
+                        return 0;
+                }
+        //      cout << "pRIME = yESSS" << endl;
+        }
+        cout << "Prime = YESSS" << i <<  endl;
+}
+int main(){
+        int n;
+        cout << "Enter the value of n\n";
+        cin >> n;
+
+        isPrime(n);
+        return 0;
+}
